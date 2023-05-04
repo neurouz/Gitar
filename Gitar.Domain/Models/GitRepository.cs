bee493504561e8713b12b9ce550d5ea9de8ba092
Ajdin Hukara <ajdin.hukara@berg.ba>
2023-05-04 08:22:21 +0200

    final changes

--- Gitar.Domain/Models/GitRepository.cs
+++ Gitar.Domain/Models/GitRepository.cs
@@ -6,12 +6,15 @@ public class GitRepository
 {
     [JsonPropertyName("id")]
     public long RepositoryId { get; set; }
+    public string? Name { get; set; }
     [JsonPropertyName("html_url")]
     public string? Url { get; set; }
     public string? Description { get; set; }
     public bool Fork { get; set; }
+    [JsonPropertyName("created_at")]
     public DateTime CreateDate { get; set; }
-    public DateTime ModifiyDate { get; set; }
+    [JsonPropertyName("pushed_at")]
+    public DateTime PushDate { get; set; }
     public string? Language { get; set; }
     [JsonPropertyName("forks")]
     public int ForkCount { get; set; }
